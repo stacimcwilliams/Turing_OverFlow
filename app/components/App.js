a82e1144a1edff28b3e165c7d549ce554441dd0c
Michael Fenwick <mickyfen17@aol.com>
2017-05-28 17:32:40 -0600

    add AskQuestion component and Route path

--- app/components/App.js
+++ app/components/App.js
@@ -4,13 +4,15 @@ import { Route, Redirect } from 'react-router-dom';
 import NavBar from './NavBar'
 import DashboardContainer from '../containers/DashboardContainer';
 import QuestionDetail from './QuestionView/QuestionDetail'
+import AskQuestion from './MainView/AskQuestion'
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <Route exact path='/' component={DashboardContainer} />
-      <Route exact path='/question' component={QuestionDetail}/>
+      <Route exact path='/' component={ DashboardContainer } />
+      <Route exact path='/question' component={ QuestionDetail }/>
+      <Route exact path='/ask-question' component={ AskQuestion }/>
     </div>
   );
 };
---
+++ app/components/MainView/AskQuestion.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AskQuestion = () => {
+
+  return (
+    <h1>Ask Question</h1>
+  );
+};
+
+export default AskQuestion;
