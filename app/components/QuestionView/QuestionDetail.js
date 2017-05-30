c5de3483207fc738bb3a08fcc41366212b06c5d4
Staci McWilliams <stacinicole86@gmail.com>
2017-05-30 16:54:30 -0600

    Add route for single question

--- app/components/QuestionView/QuestionDetail.js
+++ app/components/QuestionView/QuestionDetail.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const QuestionDetail = ({ questionID }) => {
   return (
-    <h1> Here is that great question {questionID} </h1>
+    <h1> {questionID} </h1>
   )
-}
+};
 
 export default QuestionDetail
