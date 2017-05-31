e79d5f9e9933b4debb9f9a42c947c571d2a5af40
Kyle Zucker <zkyle12@gmail.com>
2017-05-31 16:35:23 -0600

    Add skeleton for fetching answers, put in placeholder question display

--- app/components/QuestionView/QuestionDetail.js
+++ app/components/QuestionView/QuestionDetail.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
+import Button from '../Button'
 
 
 export default class QuestionDetail extends Component {
   constructor() {
     super()
+    this.state ={ 
+    	title: '',
+    	user_name: '',
+    	answers: '',
+    	answersArray:[],
+    	views: '',
+    	votes: '',
+    	created_at: '',
+    	id: '',
+    }
   }
+
   render() {
-    return(
-      <h1>{ this.props.questionID }</h1>
+  	const { title, question, user_name, answers, views, votes, created_at, id } = this.state
+    return (
+    	<div>
+     	 	<h1>{ title }</h1>
+      	<p>{ user_name }</p>
+      	<p> { question } </p>
+      	<Button name={'Answer!'}/>
+      </div>
     )
+  }
+
+  fetchAnswers() {
+  	//fetch the anwsers here
+  	const answersID = this.state.anwsers
+  	// return fetch(bla bla bla)
+  }
+
+  fetchQuestion() {
+  	//return fetch(ble bla)
+  }
+
+
+  componentDidMount () {
+  	const { questions, id } = this.props
+  	if(questions) {
+  		const q = questions.find(q => {
+  			return q.id *1 === id *1
+  		})
+  		this.setState(q)
+  		fetchAnswers().then(anwsers => this.setState({answersArray}))			
+  	} else {
+  		// fetch the specific question
+  		fetchQuestion().then(fetchAnswers).then(anwsers => this.setState({answersArray}))		
+  	}
+
+
   }
 }
