38f82bd8ce21fb68613aa6a663fdc489b0172cfd
Michael Fenwick <mickyfen17@aol.com>
2017-05-30 19:43:03 -0600

    add global error display preferences

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/stackslide.css';
+
+
 import NavBar from './NavBar'
 import DashboardContainer from '../containers/DashboardContainer';
 import AskQuestionContainer from '../containers/AskQuestionContainer';
@@ -14,7 +19,14 @@ const App = () => {
       <Route exact path='/ask-question' component={ AskQuestionContainer }/>
       <Route path="/question/:id" render={ ({ match }) =>  {
         return <QuestionDetailContainer id={match.params.id} />
-      }} />
+      }}
+      />
+      <Alert
+        stack={{ limit: 5 }}
+        position="top"
+        timeout={ 5000 }
+        effect='stackslide'
+      />
     </div>
   );
 };
