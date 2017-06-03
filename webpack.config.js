033bface05ceeaad2a4a5b48d068ceab4e0416a5
Staci McWilliams <stacinicole86@gmail.com>
2017-06-03 13:23:34 -0600

    Update entry server path in webpack config to './server/main.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devtool: '#source-map',
   context: __dirname,
   entry: [
-    './app/index.js',
+    './server/main.js',
   ],
   output: {
     path: path.join(__dirname, 'public'),
