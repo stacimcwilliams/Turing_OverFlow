f9ac3fe13e4ea574e0791c85bb579d0d24fd0c31
Staci McWilliams <stacinicole86@gmail.com>
2017-06-04 11:52:38 -0600

    Modify webpack entry path for heroku troubleshooting

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devtool: '#source-map',
   context: __dirname,
   entry: [
-    './app/index.js',
+    'app/index.js',
   ],
   output: {
     path: path.join(__dirname, 'public'),
