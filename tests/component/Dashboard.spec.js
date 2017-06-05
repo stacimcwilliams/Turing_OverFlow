fa45c1f057cf18840beff855d4ec34901e7fab81
Kyle Zucker <zkyle12@gmail.com>
2017-06-05 16:35:41 -0600

    add Dashboard test

---
+++ tests/component/Dashboard.spec.js
@@ -0,0 +1,22 @@
+process.env.NODE_ENV = 'test';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon'
+
+const chai = require('chai');
+const should = chai.should();
+const { expect } = require('chai');
+
+import Dashboard from "../../app/components/MainView/Dashboard.js"
+
+
+describe('Dashboard testing', () => {
+  it('connects', () => {
+  	const spy = sinon.spy()
+    const w = shallow(<Dashboard fetchAllQuestions={spy} questions= {[]}/>)
+
+    expect(w.find('.dashboard').length, 1).equal(1)
+  })
+})
+
