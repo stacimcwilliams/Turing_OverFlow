c4ad458eb997f762f3e61523d811a2a984b3f7d2
Michael Fenwick <mickyfen17@aol.com>
2017-06-05 20:14:26 -0600

    add end point for tags search

--- server/routes.js
+++ server/routes.js
@@ -169,15 +169,29 @@ router.patch('/questions/:id', (request, response) => {
 });
 
 router.get('/search/:searchTerm', (request, response) => {
-  // what about tags?
-  // ILIKE is case-insensitve LIKE, postgres only
   const { searchTerm } = request.params;
 
   database('questions')
   .where('question', 'ILIKE', `%${searchTerm}%`)
   .orWhere('title', 'ILIKE', `%${searchTerm}%`)
-  .then(results => {
-    response.status(200).json(results);
+  .then(searchResults => {
+    const convertedSearch = utils.alterTimeStamp(searchResults);
+    response.status(200).json(convertedSearch);
+  })
+  .catch(error => {
+    response.status(500).send({ error });
+  });
+});
+
+router.get('/search/tag/:tag', (request, response) => {
+  const { tag } = request.params;
+
+  database('tags')
+  .join('questions', 'tags.question_id', '=', 'questions.id')
+  .where('tag', 'ILIKE', `%${tag}%`).select()
+  .then(tagMatches => {
+    const convertedMatches = utils.alterTimeStamp(tagMatches);
+    response.status(200).json(convertedMatches);
   })
   .catch(error => {
     response.status(500).send({ error });
