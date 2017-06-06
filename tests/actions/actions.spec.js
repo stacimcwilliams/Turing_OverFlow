65f76a89a32a2b508d8c9167109b79b793e095d1
Staci McWilliams <stacinicole86@gmail.com>
2017-06-06 16:20:01 -0600

    Action test for search results passing

--- tests/actions/actions.spec.js
+++ tests/actions/actions.spec.js
@@ -24,15 +24,26 @@ describe('actions', () => {
   };
 
   const popularQuestions = {
-    results: [{
-      title: 'How do I convert a number to a string in JavaScript?',
-      question: 'I\'m trying to find the best way to convert a number to a string in JavaScript. Any thoughts would be great.',
-      user_name: 'Staci McWilliams',
-      views: 15,
-      answers: 1,
-      votes: 7,
-      id: 1001,
-    }],
+    results: [
+      {
+        title: 'How do I convert a number to a string in JavaScript?',
+        question: 'I\'m trying to find the best way to convert a number to a string in JavaScript. Any thoughts would be great.',
+        user_name: 'Staci McWilliams',
+        views: 15,
+        answers: 1,
+        votes: 7,
+        id: 1001,
+      },
+      {
+        title: 'How do I get a random number in JavaScript?',
+        question: 'I\'m trying to find a way to generate a random number with JavaScript. Any advice much be great.',
+        user_name: 'Mike Fenwick',
+        views: 11,
+        answers: 1,
+        votes: 4,
+        id: 1000,
+      },
+    ],
   };
 
   const recentTags = {
@@ -43,10 +54,32 @@ describe('actions', () => {
     ],
   };
 
+  const searchResults = {
+    results: [
+      {
+        title: 'How do I convert a number to a string in JavaScript?',
+        question: 'I\'m trying to find the best way to convert a number to a string in JavaScript. Any thoughts would be great.',
+        user_name: 'Staci McWilliams',
+        views: 15,
+        answers: 1,
+        votes: 7,
+        id: 1001,
+      },
+      {
+        title: 'How do I get a random number in JavaScript?',
+        question: 'I\'m trying to find a way to generate a random number with JavaScript. Any advice much be great.',
+        user_name: 'Mike Fenwick',
+        views: 11,
+        answers: 1,
+        votes: 4,
+        id: 1000,
+      },
+    ],
+  };
+
   afterEach(() => {
     store.clearActions();
   });
-
 
   it('fetchAllQuestionsAction', () => {
     const expectedAction = { type: 'FETCH_ALL_QUESTIONS', questions: mockQuestion.results };
@@ -67,6 +100,14 @@ describe('actions', () => {
   it('fetchRecentTagsAction', () => {
     const expectedAction = { type: 'FETCH_RECENT_TAGS', recentTags: recentTags.results };
     store.dispatch(actions.fetchRecentTagsAction(recentTags.results));
+    const createdAction = store.getActions();
+    expect(createdAction[0]).to.deep.equal(expectedAction);
+    expect(createdAction).to.have.lengthOf(1);
+  });
+
+  it('searchResults', () => {
+    const expectedAction = { type: 'ADD_SEARCH_RESULTS', searchResults: searchResults.results };
+    store.dispatch(actions.searchResults(searchResults.results));
     const createdAction = store.getActions();
     expect(createdAction[0]).to.deep.equal(expectedAction);
     expect(createdAction).to.have.lengthOf(1);
