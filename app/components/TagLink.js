c64ddb36bddad8dcdeda535bc9ca247a85f9207c
Michael Fenwick <mickyfen17@aol.com>
2017-06-06 11:16:57 -0600

    add searchDB & push onClick event to Tag

--- app/components/TagLink.js
+++ app/components/TagLink.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const TagLink = ({ name, added, handleDelete }) => {
+const TagLink = ({ name, added, handleDelete, fetchTag, history }) => {
+
+  const searchDB = () => {
+    fetchTag(name).then(() => history.push(`/search/tag/${name}`));
+  };
+
   return (
-    <a className="tag-link">
+    <a className="tag-link" onClick={ () => searchDB() }>
       { name }
       { added === 'ask' &&
         (<span
