9f01effc7bd2fbcae9b54afb7e2d14fbd91e803e
Michael Fenwick <mickyfen17@aol.com>
2017-06-27 16:07:01 -0600

    create new instance of auth0 and pass to Nav and Dash

--- app/components/App.js
+++ app/components/App.js
@@ -5,12 +5,16 @@ import Alert from 'react-s-alert';
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/stackslide.css';
 
-
 import NavBar from './NavBar';
 import DashboardContainer from '../containers/DashboardContainer';
 import AskQuestionContainer from '../containers/AskQuestionContainer';
 import QuestionDetailContainer from '../containers/QuestionDetailContainer';
 import SearchResultsContainer from '../containers/SearchResultsContainer';
+
+import config from '../../config.json';
+import Auth from '../Auth/auth';
+
+const auth = new Auth(config.CLIENT_ID, config.DOMAIN);
 
 export default class App extends Component {
 
@@ -20,34 +24,40 @@ export default class App extends Component {
   }
 
   render() {
+    const { userLogout } = this.props;
     return (
       <div>
-        <NavBar />
-        <Route exact path='/' component={ DashboardContainer } />
+        <NavBar auth={ auth } userLogout={ userLogout }/>
         <Route exact path='/ask-question' component={ AskQuestionContainer }/>
+        <Route exact path="/" render={ () => {
+          return <DashboardContainer auth={ auth } />;
+        }}
+        />
         <Route path="/question/:id" render={ ({ match }) => {
           return <QuestionDetailContainer id={ match.params.id } history={ history } />;
         }}
-      />
-      <Route exact path="/search/:searchTerm" render={ ({ match, location }) => {
-        return <SearchResultsContainer
-          location={ location }
-          searchTermMatch={ match.params.searchTerm }
-        />;
-      }}/>
-      <Route exact path="/search/tag/:tag" render={ ({ match, location }) => {
-        return <SearchResultsContainer
-          location={ location }
-          searchTermMatch={ match.params.tag }
-        />;
-      }}/>
-      <Alert
-        stack={{ limit: 5 }}
-        position="top"
-        timeout={ 5000 }
-        effect='stackslide'
-      />
-    </div>
+        />
+        <Route exact path="/search/:searchTerm" render={ ({ match, location }) => {
+          return <SearchResultsContainer
+            location={ location }
+            searchTermMatch={ match.params.searchTerm }
+          />;
+        }}
+        />
+        <Route exact path="/search/tag/:tag" render={ ({ match, location }) => {
+          return <SearchResultsContainer
+            location={ location }
+            searchTermMatch={ match.params.tag }
+          />;
+        }}
+        />
+        <Alert
+          stack={{ limit: 5 }}
+          position="top"
+          timeout={ 5000 }
+          effect='stackslide'
+        />
+      </div>
     );
   }
 }
