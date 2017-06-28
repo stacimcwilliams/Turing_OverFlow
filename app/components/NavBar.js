9776bdbd8db84f45eccf1822f444d8d9709ad774
Michael Fenwick <mickyfen17@aol.com>
2017-06-27 20:00:01 -0600

    add login & logout button

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 
 import Search from '../containers/SearchContainer';
 
-const NavBar = () => {
+const NavBar = ({ auth, userLogout }) => {
+  const loggedIn = auth.loggedIn();
   return (
     <div className={ 'nav-bar' }>
       <Link to={ '/' } className='home-link' />
+      { loggedIn ?
+          <button onClick={ () => { auth.logout(); userLogout({}); }}>Log Out</button>
+          :
+          <button onClick={ () => auth.login() }>Login</button>
+      }
       <Search />
     </div>
   );
