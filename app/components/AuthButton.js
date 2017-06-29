8ed699a879bee3a51d99c0a54f728755ad753547
Michael Fenwick <mickyfen17@aol.com>
2017-06-28 19:51:03 -0600

    add AutButton to check for user loginon click

---
+++ app/components/AuthButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AuthButton = ({ handleClick, className, name, btnName, auth }) => {
+  const handleAuth = (e) => {
+    if (!auth.loggedIn()) {
+      auth.login();
+    } else {
+      handleClick(e);
+    }
+  };
+  return (
+    <button
+      className={ `btn ${className}` }
+      name={ name }
+      onClick={ (e) => handleAuth(e) }>
+      { btnName }
+    </button>
+  );
+};
+
+export default AuthButton;
